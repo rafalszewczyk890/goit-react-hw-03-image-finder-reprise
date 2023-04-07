f8020b95f24c257d22bc0ed53d70c663d2161557
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-07 14:42:56 +0200

    Added loader

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import styles from './ImageGallery.module.css';
+import Loader from 'components/Loader/Loader';
 
 const API_KEY = '33215953-674c55a945dec9bfe68981b61';
 axios.defaults.baseURL = 'https://pixabay.com/api';
@@ -9,13 +10,16 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 export default class ImageGallery extends Component {
   state = {
     images: [],
+    isLoading: false,
   };
 
   async componentDidMount() {
+    this.setState({ isLoading: true });
     const response = await axios.get(
       `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
     this.setState({ images: response.data.hits });
+    this.setState({ isLoading: false });
     if (response.data.hits.length < 12) {
       this.props.onNoMoreLoad();
     }
@@ -23,10 +27,12 @@ export default class ImageGallery extends Component {
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.query !== this.props.query) {
+      this.setState({ isLoading: true });
       const response = await axios.get(
         `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
       this.setState({ images: response.data.hits });
+      this.setState({ isLoading: false });
       if (response.data.hits.length < 12) {
         this.props.onNoMoreLoad();
       }
@@ -34,12 +40,14 @@ export default class ImageGallery extends Component {
     }
 
     if (this.props.page !== prevProps.page) {
+      this.setState({ isLoading: true });
       const response = await axios.get(
         `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
       this.setState(prevState => {
         return { images: [...prevState.images, ...response.data.hits] };
       });
+      this.setState({ isLoading: false });
       if (response.data.hits.length < 12) {
         this.props.onNoMoreLoad();
       }
@@ -49,17 +57,21 @@ export default class ImageGallery extends Component {
   render() {
     const { images } = this.state;
     return (
-      <ul className={styles.ImageGallery}>
-        {images.map(image => {
-          return (
-            <ImageGalleryItem
-              id={image.id}
-              webformatURL={image.webformatURL}
-              largeImageURL={image.largeImageURL}
-            />
-          );
-        })}
-      </ul>
+      <>
+        {' '}
+        <ul className={styles.ImageGallery}>
+          {this.state.isLoading && <Loader />}
+          {images.map(image => {
+            return (
+              <ImageGalleryItem
+                id={image.id}
+                webformatURL={image.webformatURL}
+                largeImageURL={image.largeImageURL}
+              />
+            );
+          })}
+        </ul>
+      </>
     );
   }
 }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { Audio } from 'react-loader-spinner';
 
 export default class Loader extends Component {
   render() {
-    return <div>Loading...</div>;
+    return (
+      <div
+        style={{
+          backgroundColor: 'grey',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          padding: '20px',
+          borderRadius: '5px',
+        }}
+      >
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="white"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+      </div>
+    );
   }
 }
