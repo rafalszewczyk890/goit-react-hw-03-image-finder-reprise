6dc504e7713d241295b498c363a8d71067ecf875
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-08 14:30:23 +0200

    App refactor done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Button from './Button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -6,54 +6,63 @@ import Modal from './Modal/Modal';
 import Searchbar from './Searchbar/Searchbar';
 import styles from './App.module.css';
 
-export class App extends Component {
-  state = {
-    query: 'corgi',
-    page: 1,
-    showButton: true,
-    showModal: false,
-    modalPic: '',
-  };
+export const App = () => {
+  // state = {
+  //   query: 'corgi',
+  //   page: 1,
+  //   showButton: true,
+  //   showModal: false,
+  //   modalPic: '',
+  // };
 
-  onSubmit = (event, query) => {
+  const [query, setQuery] = useState('corgi');
+  const [page, setPage] = useState(1);
+  const [showButton, setShowButton] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [modalPic, setModalPic] = useState('');
+
+  const onSubmit = (event, query) => {
     event.preventDefault();
-    this.setState({ query: query, page: 1, showButton: true });
+    // this.setState({ query: query, page: 1, showButton: true });
+    setQuery(query);
+    setPage(1);
+    setShowButton(true);
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const onLoadMore = () => {
+    // this.setState(prevState => {
+    //   return { page: prevState.page + 1 };
+    // });
+    setPage(page + 1);
   };
 
-  hideButton = () => {
-    this.setState({ showButton: false });
+  const hideButton = () => {
+    // this.setState({ showButton: false });
+    setShowButton(false);
   };
 
-  showModal = largeImageURL => {
-    this.setState({ showModal: true, modalPic: largeImageURL });
+  const showModalFunc = largeImageURL => {
+    // this.setState({ showModal: true, modalPic: largeImageURL });
+    setShowModal(true);
+    setModalPic(largeImageURL);
   };
 
-  hideModal = () => {
-    this.setState({ showModal: false });
+  const hideModal = () => {
+    // this.setState({ showModal: false });
+    setShowModal(false);
   };
 
-  render() {
-    return (
-      <div className={styles.App}>
-        <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery
-          query={this.state.query}
-          page={this.state.page}
-          onLoadMore={this.state.loadMore}
-          onNoMoreLoad={this.hideButton}
-          onClick={this.showModal}
-        />
-        {this.state.showButton && <Button onLoadMore={this.onLoadMore} />}
-        {this.state.showModal && (
-          <Modal onClick={this.hideModal} src={this.state.modalPic} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.App}>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery
+        query={query}
+        page={page}
+        onNoMoreLoad={hideButton}
+        onClick={showModalFunc}
+      />
+      {showButton && <Button onLoadMore={onLoadMore} />}
+      {showModal && <Modal onClick={hideModal} src={modalPic} />}
+    </div>
+  );
+};
