5ba50c05675f5ba1a0dc16858a6a60433cf1fe6b
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-09 11:32:31 +0200

    Fixed bug with state reload when loading more

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import styles from './ImageGallery.module.css';
 import Loader from 'components/Loader/Loader';
-
-const API_KEY = '33215953-674c55a945dec9bfe68981b61';
-axios.defaults.baseURL = 'https://pixabay.com/api';
+import { fetchImages } from 'components/api/api';
 
 export const ImageGallery = props => {
   const [images, setImages] = useState([]);
@@ -15,49 +12,20 @@ export const ImageGallery = props => {
 
   useEffect(() => {
     setIsLoading(true);
-    const fetchImages = async () => {
-      const response = await axios.get(
-        `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      setImages(response.data.hits);
-      setIsLoading(false);
-      if (response.data.hits.length < 12) {
-        onNoMoreLoad();
-      }
-    };
-    fetchImages();
+    fetchImages(query, page, onNoMoreLoad, setImages);
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
     setIsLoading(true);
-    const fetchImages = async () => {
-      const response = await axios.get(
-        `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      setImages(response.data.hits);
-      setIsLoading(false);
-      if (response.data.hits.length < 12) {
-        onNoMoreLoad();
-      }
-    };
-    fetchImages();
+    fetchImages(query, page, onNoMoreLoad, setImages);
+    setIsLoading(false);
   }, [query]);
 
   useEffect(() => {
-    if (page > 1) {
-      setIsLoading(true);
-      const fetchImages = async () => {
-        const response = await axios.get(
-          `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        setImages([...images, ...response.data.hits]);
-        setIsLoading(false);
-        if (response.data.hits.length < 12) {
-          onNoMoreLoad();
-        }
-      };
-      fetchImages();
-    }
+    setIsLoading(true);
+    fetchImages(query, page, onNoMoreLoad, setImages, images);
+    setIsLoading(false);
   }, [page]);
 
   return (
---
+++ src/components/api/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const API_KEY = '33215953-674c55a945dec9bfe68981b61';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export const fetchImages = async (
+  query,
+  page,
+  onNoMoreLoad,
+  setImages,
+  images
+) => {
+  const response = await axios.get(
+    `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  if (response.data.hits.length < 12) {
+    onNoMoreLoad();
+  }
+  if (page > 1) {
+    setImages([...images, ...response.data.hits]);
+  } else {
+    setImages(response.data.hits);
+  }
+  console.log(response.data.hits);
+};
