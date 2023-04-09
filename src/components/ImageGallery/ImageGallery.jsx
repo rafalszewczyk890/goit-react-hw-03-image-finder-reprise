595af8a4b970f7b7049426eb5e154ebad5aa0a2e
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-09 10:53:33 +0200

    Fixed bug

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -11,18 +11,6 @@ export const ImageGallery = props => {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // async componentDidMount() {
-  //   this.setState({ isLoading: true });
-  //   const response = await axios.get(
-  //     `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  //   );
-  //   this.setState({ images: response.data.hits });
-  //   this.setState({ isLoading: false });
-  //   if (response.data.hits.length < 12) {
-  //     this.props.onNoMoreLoad();
-  //   }
-  // }
-
   const { query, page, onNoMoreLoad, onClick } = props;
 
   useEffect(() => {
@@ -56,48 +44,21 @@ export const ImageGallery = props => {
   }, [query]);
 
   useEffect(() => {
-    setIsLoading(true);
-    const fetchImages = async () => {
-      const response = await axios.get(
-        `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      setImages([...images, ...response.data.hits]);
-      setIsLoading(false);
-      if (response.data.hits.length < 12) {
-        onNoMoreLoad();
-      }
-    };
-    fetchImages();
+    if (page > 1) {
+      setIsLoading(true);
+      const fetchImages = async () => {
+        const response = await axios.get(
+          `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        setImages([...images, ...response.data.hits]);
+        setIsLoading(false);
+        if (response.data.hits.length < 12) {
+          onNoMoreLoad();
+        }
+      };
+      fetchImages();
+    }
   }, [page]);
-
-  // async componentDidUpdate(prevProps, prevState) {
-  //   if (prevProps.query !== this.props.query) {
-  //     this.setState({ isLoading: true });
-  //     const response = await axios.get(
-  //       `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  //     );
-  //     this.setState({ images: response.data.hits });
-  //     this.setState({ isLoading: false });
-  //     if (response.data.hits.length < 12) {
-  //       this.props.onNoMoreLoad();
-  //     }
-  //     return;
-  //   }
-
-  //   if (this.props.page !== prevProps.page) {
-  //     this.setState({ isLoading: true });
-  //     const response = await axios.get(
-  //       `/?q=${this.props.query}&page=${this.props.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  //     );
-  //     this.setState(prevState => {
-  //       return { images: [...prevState.images, ...response.data.hits] };
-  //     });
-  //     this.setState({ isLoading: false });
-  //     if (response.data.hits.length < 12) {
-  //       this.props.onNoMoreLoad();
-  //     }
-  //   }
-  // }
 
   return (
     <>
